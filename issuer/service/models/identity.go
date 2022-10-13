68c4b15457993c602c010413f83a1b8d51fa361f
Ron Kahat <ron.kahat@gmail.com>
2022-10-13 19:56:04 +0300

    fix: create models in service and refactor existing models

--- issuer/service/models/identity.go
+++ issuer/service/models/identity.go
@@ -1,20 +1,16 @@
 package models
 
-import core "github.com/iden3/go-iden3-core"
-
-// Identity model
 type Identity struct {
-	Identifier string
-	State      IdentityState
+	//Identifier string
+	//State      IdentityState
 }
 
-// NewIdentityFromIdentifier default identity model from identity and root state
-func NewIdentityFromIdentifier(id *core.ID, rootState string) *Identity {
-	return &Identity{
-		Identifier: id.String(),
-		State: IdentityState{
-			Identifier: id.String(),
-			State:      &rootState,
-		},
-	}
-}
+//func NewIdentityFromIdentifier(id *core.ID, rootState string) *Identity {
+//	return &Identity{
+//		Identifier: id.String(),
+//		State: IdentityState{
+//			Identifier: id.String(),
+//			State:      &rootState,
+//		},
+//	}
+//}
--- issuer/service/models/identityState.go
+++ issuer/service/models/identityState.go
@@ -1,24 +1,17 @@
 package models
 
-import (
-	"github.com/iden3/go-circuits"
-)
-
-// IdentityStatus represents type for state Status
 type IdentityStatus string
 
-// IdentityState identity state model
 type IdentityState struct {
-	StateID            int64
-	Identifier         string
-	State              *string
-	RootOfRoots        *string
-	ClaimsTreeRoot     *string
-	RevocationTreeRoot *string
+	//StateID            int64
+	//Identifier         string
+	//State              *string
+	//RootOfRoots        *string
+	//ClaimsTreeRoot     *string
+	//RevocationTreeRoot *string
 }
 
-// ToTreeState returns circuits.TreeState structure
-func (is IdentityState) ToTreeState() (circuits.TreeState, error) {
-	return BuildTreeState(is.State, is.ClaimsTreeRoot, is.RevocationTreeRoot,
-		is.RootOfRoots)
-}
+//func (is IdentityState) ToTreeState() (circuits.TreeState, error) {
+//	return BuildTreeState(is.State, is.ClaimsTreeRoot, is.RevocationTreeRoot,
+//		is.RootOfRoots)
+//}
