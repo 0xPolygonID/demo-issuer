bdee935b9e617e332952d01bc6c59dbd0f2218de
Ron Kahat <ron.kahat@gmail.com>
2022-10-16 22:17:56 +0300

    fix: service factory update

--- issuer/service/factory.go
+++ issuer/service/factory.go
@@ -3,6 +3,10 @@ package service
 import (
 	logger "github.com/sirupsen/logrus"
 	"issuer/service/cfgs"
+	"issuer/service/claims"
+	database "issuer/service/db"
+	"issuer/service/http"
+	"issuer/service/identity"
 	"os"
 	"time"
 )
@@ -22,11 +26,31 @@ func CreateApp(altCfgPath string) error {
 
 	// create dependencies
 
+	db, err := database.New(cfg.DB.FilePath)
+	if err != nil {
+		return err
+	}
+
 	// create main components
+	// identity
+	// claims
+	// agent
+
+	claimsManager, err := claims.New()
+	if err != nil {
+		return err
+	}
+
+	identityManager, err := identity.New(db, )
+	if err != nil {
+		return err
+	}
+
+	//agent, err := agent
 
 	// init an api server
-
-	return nil
+	s := http.NewServer(cfg.HttpListenAddress, identityManager, claimsManager)
+	return s.Run()
 }
 
 func initGlobalLogger(level string) error {
