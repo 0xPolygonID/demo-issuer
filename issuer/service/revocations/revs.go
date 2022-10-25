f286422100fd128a149017c21e3d216fd4ac0b06
Ron Kahat <ron.kahat@gmail.com>
2022-10-26 00:45:55 +0300

    feat: create roots and revos state managers

---
+++ issuer/service/revocations/revs.go
@@ -0,0 +1,36 @@
+package revocations
+
+import (
+	"context"
+	store "github.com/demonsh/smt-bolt"
+	"github.com/iden3/go-merkletree-sql"
+	"issuer/service/db"
+	"math/big"
+)
+
+type Revocations struct {
+	RevTree *merkletree.MerkleTree
+}
+
+func New(db *db.DB, treeDepth int) (*Revocations, error) {
+	treeStorage, err := store.NewBoltStorage(db.GetConnection())
+	if err != nil {
+		return nil, err
+	}
+
+	revsTree, err := merkletree.NewMerkleTree(context.Background(), treeStorage.WithPrefix([]byte("revocation")), treeDepth)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Revocations{
+		RevTree: revsTree,
+	}, nil
+
+}
+
+// GenerateRevocationProof generates the proof of existence (or non-existence) of an nonce in RevocationTree
+func (r *Revocations) GenerateRevocationProof(nonce *big.Int) (*merkletree.Proof, error) {
+	proof, _, err := r.RevTree.GenerateProof(context.Background(), nonce, r.RevTree.Root())
+	return proof, err
+}
---
+++ issuer/service/roots/roots.go
@@ -0,0 +1,29 @@
+package roots
+
+import (
+	"context"
+	store "github.com/demonsh/smt-bolt"
+	"github.com/iden3/go-merkletree-sql"
+	"issuer/service/db"
+)
+
+type Roots struct {
+	RootsTree *merkletree.MerkleTree
+}
+
+func New(db *db.DB, treeDepth int) (*Roots, error) {
+
+	treeStorage, err := store.NewBoltStorage(db.GetConnection())
+	if err != nil {
+		return nil, err
+	}
+
+	roots, err := merkletree.NewMerkleTree(context.Background(), treeStorage.WithPrefix([]byte("ror")), treeDepth)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Roots{
+		RootsTree: roots,
+	}, nil
+}
